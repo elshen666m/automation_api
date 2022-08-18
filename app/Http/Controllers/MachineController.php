1df53020c57ad91c0b27f2b14ffc30d26f668910
Elshen <m.elshan@hostart.az>
2022-08-18 13:55:37 +0400

    addded post datas

--- app/Http/Controllers/MachineController.php
+++ app/Http/Controllers/MachineController.php
@@ -9,6 +9,11 @@ class MachineController extends Controller
 {
     public function create(Request $request)
     {
-        $res = Http::post('http://188.72.162.97:8080/api/add-machine', $request->post());
+        $res = Http::post('http://188.72.162.97:8080/api/add-machine', [
+            'memory' => $request->memory,
+            'disk' => $request->disk,
+            'cpu' => $request->cpu,
+            'datacenter' => $request->datacenter
+        ]);
     }
 }
