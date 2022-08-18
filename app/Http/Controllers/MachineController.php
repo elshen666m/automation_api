9d628db3d336513e32ce1c7182688b1de5b933e2
Elshen <m.elshan@hostart.az>
2022-08-18 13:28:59 +0400

    add machine request

--- app/Http/Controllers/MachineController.php
+++ app/Http/Controllers/MachineController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MachineController extends Controller
 {
     public function create(Request $request)
     {
-        Log::error($request->all());
+        Http::post('http://127.0.0.1:8080/api/add-machine', $request->post());
     }
 }
